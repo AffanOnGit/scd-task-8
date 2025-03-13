8b73da45a780fb8a890be45ec59a1174907dc7cb
AffanOnGit <affanhameed3096@gmail.com>
2025-03-13 11:21:36 +0500

    vuiy

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require('express');
+import express from 'express';
+import { config } from 'dotenv';
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const bookingRoutes = require('./routes/bookingRoutes');
+config();
 
-dotenv.config();
-
+// Replace <username>, <password>, and <dbname> with your actual credentials
+const dbURL = ;
 const app = express();
+
 app.use(express.json());
+const port = process.env.PORT ?? 3000;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+app.get('/', (req, res) => {
+    res.send('Welcome to the event management for SCD Task 8');
+});
 
-// Routes
-app.use('/api/bookings', bookingRoutes);
-
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => console.log(`Booking Service running on port ${PORT}`));
+app.listen(port, () => {
+    console.log(`Server running on: http://localhost:${port}`);
+});
